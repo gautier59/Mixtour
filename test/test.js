ca6806c3123fa303af4890edc7352fcf95fff710
Quentin vauthier <Quentin vauthier>
2015-12-08 11:11:25 +0100

    Fonction deplacer un pion

--- test/test.js
+++ test/test.js
@@ -12,10 +12,10 @@ MixTourTestCase.prototype.testInitPlateau = function() {
     monPlateau.setColorPlayer("W");
 
     console.log("Pion sur le plateau ? : "+monPlateau.pionOnPlateau());
-    monPlateau.addPion(1, 2, "W");
-    monPlateau.addPion(1, 2, "B");
-    monPlateau.addPion(1, 2, "B");
-    monPlateau.addPion(1, 2, "B");
+    monPlateau.addPion(0, 0, "W");//1
+    monPlateau.addPion(0, 0, "H");//2
+    monPlateau.addPion(0, 0, "W");//3
+    monPlateau.addPion(0, 1, "B");
     monPlateau.addPion(1, 2, "W");
     monPlateau.addPion(4, 2, "W");
     monPlateau.addPion(4, 2, "W");
@@ -26,4 +26,7 @@ MixTourTestCase.prototype.testInitPlateau = function() {
     console.log("");
     console.log(" --- CELLS EMPTY ---");
     monPlateau.getCellEmpty(); // On recupère les celulles vides
+    monPlateau.movePions(0,0,2,0,1)//Départ en (0;0) on prend les pions à partir de la hauteur 2 et on les déplacenet en (0;1)
+    monPlateau.viewListPionPosition(0,1);
+    monPlateau.viewListPionPosition(0,0);
 };
