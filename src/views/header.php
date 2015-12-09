5953a81e3ab4605cd62bde4e83866b611b2bcfa8
clouet jean-marie <clouet.jm@gmail.com>
2015-12-09 14:01:15 +0100

    Mixtour v2

--- src/views/header.php
+++ src/views/header.php
@@ -24,8 +24,22 @@
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
 				<li><a href="signup.php"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>&nbsp;Inscription</a></li>
-				<li><a href="#"><span class="glyphicon glyphicon-fire" aria-hidden="true"></span>&nbsp;Jouer</a></li>
-                <li><a href="consulter.php"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Consulter </a></li>
+				<li><a href="game.php"><span class="glyphicon glyphicon-fire" aria-hidden="true"></span>&nbsp;Jouer</a></li>
+				<li><a href="classement.php"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;Classement </a></li>
+               <?php
+               		session_start();
+              		 if(isset($_SESSION['typeUtilisateur']) == "Client"){
+               	?>
+               		 <li><a href="consulter.php"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Consulter </a></li>
+                <?php } 
+                if(isset($_SESSION['typeUtilisateur']) == "Client" || isset($_SESSION['typeUtilisateur']) == "User"){
+                ?>
+                <li><a href="deconnexion.php"><span class="glyphicon glyphicon-off" aria-hidden="true"></span>&nbsp;D&eacute;connexion </a></li>
+                <?php }?>
+                <li>
+                <div class="pull-right"><?php 
+                		if(isset($_SESSION['typeUtilisateur']))
+               				echo "Bievenue ".$_SESSION['typeUtilisateur']?></div></li>
 			</ul>
 		</div>
 	</div>
