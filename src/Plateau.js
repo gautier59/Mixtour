eca304da81dcc386eeb650dd192807b1d71acd06
Martinet Maxime <maxi62330@gmail.com>
2015-12-10 09:24:14 +0100

    Ajout fonction IA + Correction bug MovePion

--- src/Plateau.js
+++ src/Plateau.js
@@ -24,8 +24,9 @@ var Plateau = function () {
 
     this.setColorPlayer = function(color){
         colorPlayer = color;
+        console.log("This color player is "+colorPlayer);
 
-        if(color = "W")
+        if(color == "W")
             colorIA = "B";
         else
             colorIA = "W";
@@ -48,6 +49,8 @@ var Plateau = function () {
         }
         else{
             console.log("[ERR] Placement impossible: La case L:"+line+" C:"+columns+" n'est pas vide");
+            this.viewListPionPosition(line,columns);
+
             return false;
         }
     };
@@ -115,7 +118,7 @@ var Plateau = function () {
             for(var o = 0; o < 5 ; o++) {
                 if (this.getListPionPosition(i,o).length == 0){
                     arrayCellEmpty.push("{"+i+";"+o+"}");
-                    console.log("---{"+i+";"+o+"}");
+                    //console.log("---{"+i+";"+o+"}");
                 }
             }
         }
@@ -129,10 +132,12 @@ var Plateau = function () {
             for (var i = 0; i < lstTmp.length; i++) {
                 lstTmp[i].line = positionEndLine;
                 lstTmp[i].column = positionEndColumn;
-                lstTmp[i].height = this.getListPionPosition(positionEndLine, positionEndColumn).length;
+                lstTmp[i].height = (this.getListPionPosition(positionEndLine, positionEndColumn).length);
                 //console.log(lstTmp[i].line + " " + lstTmp[i].column);
+                console.log("Deplacement de L:"+positionStartLine+" C:"+positionStartColumn + " H:"+lstTmp[i].height+ " vers L:"+positionEndLine+" H:"+positionEndColumn);
             }
             this.checkHeightTower(positionEndLine, positionEndColumn);
+            this.changePlayer();
             return true;
         }else{
             console.log("[ERR] Le déplacement est imposible la cellule est VIDE !");
@@ -178,15 +183,15 @@ var Plateau = function () {
 
     this.deleteTower = function(line, column){
         var tower = this.getListPionPosition(line,column);
-        for(var i = 0; i < listPion.length ; i++){
-            for(var j = 0; j < tower.length ; j++){
-                if(listPion[i] == tower[j]){
-                    console.log("SUPPRESSION L:"+listPion[i].line+" C:"+listPion[i].column+" H:"+listPion[i].height+" C:"+listPion[i].color);
+
+        for(var i = 0; i < tower.length ; i++){
+            for(var j = 0; j < listPion.length ; j++){
+                if(tower[i] == listPion[j]){
                     listPion.splice(j,1);
                 }
             }
         }
-    }
+    };
 
     this.checkMovePion = function(startLine, startColumn, endLine, endColumn) {
         console.log("Taille : " + this.getListPionPosition(endLine,endColumn).length)
@@ -212,4 +217,123 @@ var Plateau = function () {
         console.log("Aucune correspondance");
         return false;
     }
+
+    this.checkPlayerProgressIsIA = function(){
+        if(playerProgress == colorIA) {
+            var result = this.generateGameIA();
+            console.log("IA generer:"+result);
+            return result;
+        }
+        else
+            return -1;
+    };
+
+    this.generateGameIA = function(){
+
+
+
+            //console.log("   ---   IA DEPLACER PIONS   ---   ");
+
+
+            var towerLowerLine = -1;
+            var towerLowerColumn = -1;
+            var towerHigherLine = -1;
+            var towerHigherColumn = -1;
+            var towerHigherHeight = -1;
+            var tmp_towerLowerHeight = -1;
+
+            var cellTowerSame = this.getTowerSameColorFirstPion("W");
+            if(cellTowerSame.length > 0) {
+                for(var i = 0; i < cellTowerSame.length ; i++){ // Permet de parcourir la liste est prendre la meilleure solution
+                    if(cellTowerSame[i].charAt(5) > towerHigherHeight){
+                        towerHigherLine = cellTowerSame[i].charAt(1);
+                        towerHigherColumn = cellTowerSame[i].charAt(3);
+                        towerHigherHeight = cellTowerSame[i].charAt(5);
+                    }
+                }
+                //var randomSelectTowerHigher = getRandomInt(0, cellTowerSame.length);
+                //towerHigherLine = cellTowerSame[randomSelectTowerHigher].charAt(1);
+                //towerHigherColumn = cellTowerSame[randomSelectTowerHigher].charAt(3);
+                //towerHigherHeight = cellTowerSame[randomSelectTowerHigher].charAt(5);
+                console.log(" - TOWER HIGHER SELECT IS L:" + towerHigherLine + " C:" + towerHigherColumn + " avec H:" + towerHigherHeight);
+
+
+                /// Récupèration de notre TOWER basse
+                var cellTowerMore = this.getTowerWithMorePionParameter(5 - towerHigherHeight , towerHigherLine , towerHigherColumn);
+                if(cellTowerMore.length > 0) {
+                    for(var i = 0; i < cellTowerMore.length ; i++){ // Permet de parcourir la liste est prendre la meilleure solution
+                        if(cellTowerMore[i].charAt(5) > tmp_towerLowerHeight){
+                            towerLowerLine = cellTowerMore[i].charAt(1);
+                            towerLowerColumn = cellTowerMore[i].charAt(3);
+                        }
+                    }
+                    //var randomSelectTowerLower = getRandomInt(0,cellTowerMore.length);
+                    //towerLowerLine = cellTowerMore[randomSelectTowerLower].charAt(1);
+                    //towerLowerColumn = cellTowerMore[randomSelectTowerLower].charAt(3);
+                    console.log(" - TOWER LOWER SELECT IS L:" + towerLowerLine + " C:" + towerLowerColumn);
+
+                    this.movePions(towerHigherLine,towerHigherColumn, 0 ,towerLowerLine ,towerLowerColumn);
+                    return "{d:"+towerHigherLine+":"+towerHigherColumn+":"+ 0 +":"+towerLowerLine +":"+towerLowerColumn+"}";
+
+                }else{
+                    console.log("NOUS N'AVOUS PAS DE TOUR QUI PERMETTENT DE PRENDRE UN POINT DIRECTEMENT");
+
+                    console.log("  ----   IA PLACER PIONS   ---   ");
+                    var cellsEmpty = this.getCellEmpty();
+                    var rand = getRandomInt(0,cellsEmpty.length);
+                    this.addPion(cellsEmpty[rand].charAt(1), cellsEmpty[rand].charAt(3), colorIA);
+                    return "{p:"+cellsEmpty[rand].charAt(1)+":"+cellsEmpty[rand].charAt(3)+"}";
+
+                }
+
+
+            }
+            else{
+                console.log("NOUS N'AVONS PAS DE TOUR AVEC NOTRE COULEUR EN HAUT");
+
+                console.log("  ----   IA PLACER PIONS   ---   ");
+                var cellsEmpty = this.getCellEmpty();
+                var rand = getRandomInt(0,cellsEmpty.length);
+                this.addPion(cellsEmpty[rand].charAt(1), cellsEmpty[rand].charAt(3), colorIA);
+                return "{p:"+cellsEmpty[rand].charAt(1)+":"+cellsEmpty[rand].charAt(3)+"}";
+
+            }
+    };
+
+    function getRandomInt(min, max){
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    this.getTowerSameColorFirstPion = function(color){ // Retourne une liste de cellules ayant des tour donc le pion le plus haut est celui de ca couleur : {0;0;3)  = {L;C;HauteurTotal}
+        var arrayCellWithTowerSameColorFirstPion = [];
+
+        for(var i = 0; i < 5 ; i++){
+            for(var j = 0 ; j < 5; j++){
+                if(this.getListPionPosition(i,j).length > 0){ // On est sur une cells qui contient une tour
+                    var pionCell = this.getListPionPosition(i,j);
+                    if(pionCell[pionCell.length-1].color == playerProgress){ // Compare le pion le plus haut avec la couleur du joueur
+                        arrayCellWithTowerSameColorFirstPion.push("{"+i+";"+j+";"+pionCell.length+"}");
+                    }
+                }
+            }
+        }
+        return arrayCellWithTowerSameColorFirstPion;
+    }
+
+    this.getTowerWithMorePionParameter = function(heightmin , lineTowerHigher, columnTowerHigher){ // Récupère la liste des cells avec des colonnes qui ont au minimum la hauteur passer en paramètre et différent de la cell de la tour haute
+        var arrayCellWithMorePionParameter = [];
+
+        for(var i = 0; i < 5 ; i++) {
+            for (var j = 0; j < 5; j++) {
+                if(this.getListPionPosition(i,j).length >= heightmin) { // On est sur une cells qui contient une tour
+                    if ((i != lineTowerHigher) || (j != columnTowerHigher)) { // Vérifier si on est pas sur la même cellules que la tour haute !
+                        var pionCell = this.getListPionPosition(i, j);
+                            arrayCellWithMorePionParameter.push("{" + i + ";" + j + ";"+pionCell.length+"}");
+                    }
+                }
+            }
+        }
+
+        return arrayCellWithMorePionParameter;
+    };
 };
