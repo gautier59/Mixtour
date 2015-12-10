db50ea361f31dcca69c467ce06605d0b130579a8
Quentin vauthier <Quentin vauthier>
2015-12-10 09:39:45 +0100

    Fonction deplacer pion avance termine

--- src/Plateau.js
+++ src/Plateau.js
@@ -4,212 +4,259 @@
 'use strict';
 
 var Plateau = function () {
-    var plateau;
-    var colorPlayer = "W";
-    var colorIA = "B";
-    var listPion = [];
-    var playerProgress = "W"; // Par d�faut c'est au joueur blanc de commencer
-    var pointPlayer = 0;
-    var pointIA = 0;
+        var plateau;
+        var colorPlayer = "W";
+        var colorIA = "B";
+        var listPion = [];
+        var playerProgress = "W"; // Par défaut c'est au joueur blanc de commencer
+        var pointPlayer = 0;
+        var pointIA = 0;
+        var lastCoup = [];
 
-    this.init = function () {
-        plateau = new Array(5);
-        for (var i = 0; i < plateau.length; i++) {
-            plateau[i] = new Array(5);
-            for (var o = 0; o < 5; o++) {
-                plateau[i][o] = "o";
-            }
-        }
-    };
-
-    this.setColorPlayer = function(color){
-        colorPlayer = color;
-
-        if(color = "W")
-            colorIA = "B";
-        else
-            colorIA = "W";
-    };
-
-    this.pionOnPlateau = function(){
-        if(listPion.length != 0)
-            return true;
-        else
-            return false;
-    };
-
-    this.addPion = function(line, columns, color){
-        var nbPionCell = this.getListPionPosition(line,columns).length;
-        if(nbPionCell == 0) {
-            var pion = new Pion(line, columns, (nbPionCell + 1), color);
-            listPion.push(pion);
-            this.changePlayer();
-            return true;
-        }
-        else{
-            console.log("[ERR] Placement impossible: La case L:"+line+" C:"+columns+" n'est pas vide");
-            return false;
-        }
-    };
-
-    this.viewListPionPlateau = function(){
-        console.log("  --- PION PLACE ---");
-        console.log("            Number pion:"+listPion.length);
-        for(var i = 0; i < listPion.length ; i++) {
-            console.log("            line:" +listPion[i].line +" column:"+listPion[i].column+" height:"+listPion[i].height + " color:" + listPion[i].color);
-        }
-        console.log(" --- ---");
-    };
-
-
-    this.viewListPionPosition = function(line , column){
-        var listPionCell = [];
-
-        for(var i = 0 ; i < listPion.length ; i++){
-            if((listPion[i].line == line) && (listPion[i].column == column))
-            {
-                listPionCell.push(listPion[i]);
-            }
-        }
-        listPionCell.sort(function(a, b) {
-            if (a.height < b.height) //sort string ascending
-                return -1
-            if (a.height > b.height)
-                return 1
-            return 0 //
-        });
-
-        console.log("  --- PION PLACE (L:"+line+" C:"+column+") ---");
-        console.log("            Number pion:"+listPionCell.length);
-        for(var i = 0; i < listPionCell.length ; i++) {
-            console.log("            line:" +listPionCell[i].line +" column:"+listPionCell[i].column+" height:"+listPionCell[i].height + " color:" + listPionCell[i].color);
-        }
-        console.log(" --- ---");
-    };
-
-    this.getListPionPosition = function(line , column) {
-        var listPionCell = [];
-        var count = 1;
-
-        for (var i = 0; i < listPion.length; i++) {
-            if ((listPion[i].line == line) && (listPion[i].column == column)) {
-                listPionCell.push(listPion[i]);
-            }
-        }
-
-        listPionCell.sort(function(a, b) {
-            if (a.height < b.height) //sort string ascending
-                return -1
-            if (a.height > b.height)
-                return 1
-            return 0 //
-        });
-
-        return listPionCell;
-    };
-
-    this.getCellEmpty = function(){
-        var arrayCellEmpty = [];
-
-        for(var i = 0; i <5 ; i++){
-            for(var o = 0; o < 5 ; o++) {
-                if (this.getListPionPosition(i,o).length == 0){
-                    arrayCellEmpty.push("{"+i+";"+o+"}");
-                    console.log("---{"+i+";"+o+"}");
+        this.init = function () {
+            plateau = new Array(5);
+            for (var i = 0; i < plateau.length; i++) {
+                plateau[i] = new Array(5);
+                for (var o = 0; o < 5; o++) {
+                    plateau[i][o] = "o";
                 }
             }
-        }
-        return arrayCellEmpty;
-    };
+        };
 
-    this.movePions = function(positionStartLine, positionStartColumn,positionStartHeight, positionEndLine, positionEndColumn) {
-        if(this.getListPionPosition(positionEndLine, positionEndColumn).length != 0) {
-            var listPionTmp = this.getListPionPosition(positionStartLine, positionStartColumn);
-            var lstTmp = listPionTmp.slice(positionStartHeight - 1, listPionTmp.length + 1); //On prend 2 et 3
-            for (var i = 0; i < lstTmp.length; i++) {
-                lstTmp[i].line = positionEndLine;
-                lstTmp[i].column = positionEndColumn;
-                lstTmp[i].height = this.getListPionPosition(positionEndLine, positionEndColumn).length;
-                //console.log(lstTmp[i].line + " " + lstTmp[i].column);
-            }
-            this.checkHeightTower(positionEndLine, positionEndColumn);
-            return true;
-        }else{
-            console.log("[ERR] Le d�placement est imposible la cellule est VIDE !");
-            return false;
-        }
-        //this.checkMovePion(positionStartLine,positionStartColumn,positionEndLine,positionEndColumn)
-    };
+        this.setColorPlayer = function (color) {
+            colorPlayer = color;
 
-    this.changePlayer = function(){
-        if(playerProgress == "W")
-            playerProgress = "B";
-        else
-            playerProgress = "W";
-
-        console.log("C'est au tour du joueur :"+playerProgress);
-    };
-
-    this.getPlayerProgress = function(){
-        return playerProgress;
-    };
-
-    this.checkHeightTower = function(line , column){
-        var tower = this.getListPionPosition(line,column);
-        var colorLastPion = tower[tower.length-1].color;
-        if(tower.length >= 5){
-            if(colorLastPion == colorPlayer)
-                pointPlayer++;
+            if (color = "W")
+                colorIA = "B";
             else
-                pointIA++;
+                colorIA = "W";
+        };
 
-            console.log("Le joueur "+colorLastPion+" a gagner !!");
-            this.deleteTower(line,column);
-        }
-    };
+        this.pionOnPlateau = function () {
+            if (listPion.length != 0)
+                return true;
+            else
+                return false;
+        };
 
-    this.getScorePlayer = function(){
-        return pointPlayer;
-    };
+        this.addPion = function (line, columns, color) {
+            var nbPionCell = this.getListPionPosition(line, columns).length;
+            if (nbPionCell == 0) {
+                var pion = new Pion(line, columns, (nbPionCell + 1), color);
+                listPion.push(pion);
+                this.changePlayer();
+                return true;
+            }
+            else {
+                console.log("[ERR] Placement impossible: La case L:" + line + " C:" + columns + " n'est pas vide");
+                return false;
+            }
+        };
 
-    this.getScoreIA = function(){
-        return pointIA;
-    };
+        this.viewListPionPlateau = function () {
+            console.log("  --- PION PLACE ---");
+            console.log("            Number pion:" + listPion.length);
+            for (var i = 0; i < listPion.length; i++) {
+                console.log("            line:" + listPion[i].line + " column:" + listPion[i].column + " height:" + listPion[i].height + " color:" + listPion[i].color);
+            }
+            console.log(" --- ---");
+        };
 
-    this.deleteTower = function(line, column){
-        var tower = this.getListPionPosition(line,column);
-        for(var i = 0; i < listPion.length ; i++){
-            for(var j = 0; j < tower.length ; j++){
-                if(listPion[i] == tower[j]){
-                    console.log("SUPPRESSION L:"+listPion[i].line+" C:"+listPion[i].column+" H:"+listPion[i].height+" C:"+listPion[i].color);
-                    listPion.splice(j,1);
+
+        this.viewListPionPosition = function (line, column) {
+            var listPionCell = [];
+
+            for (var i = 0; i < listPion.length; i++) {
+                if ((listPion[i].line == line) && (listPion[i].column == column)) {
+                    listPionCell.push(listPion[i]);
+                }
+            }
+            listPionCell.sort(function (a, b) {
+                if (a.height < b.height) //sort string ascending
+                    return -1
+                if (a.height > b.height)
+                    return 1
+                return 0 //
+            });
+
+            console.log("  --- PION PLACE (L:" + line + " C:" + column + ") ---");
+            console.log("            Number pion:" + listPionCell.length);
+            for (var i = 0; i < listPionCell.length; i++) {
+                console.log("            line:" + listPionCell[i].line + " column:" + listPionCell[i].column + " height:" + listPionCell[i].height + " color:" + listPionCell[i].color);
+            }
+            console.log(" --- ---");
+        };
+
+        this.getListPionPosition = function (line, column) {
+            var listPionCell = [];
+            var count = 1;
+
+            for (var i = 0; i < listPion.length; i++) {
+                if ((listPion[i].line == line) && (listPion[i].column == column)) {
+                    listPionCell.push(listPion[i]);
+                }
+            }
+
+            listPionCell.sort(function (a, b) {
+                if (a.height < b.height) //sort string ascending
+                    return -1
+                if (a.height > b.height)
+                    return 1
+                return 0 //
+            });
+
+            return listPionCell;
+        };
+
+        this.getCellEmpty = function () {
+            var arrayCellEmpty = [];
+
+            for (var i = 0; i < 5; i++) {
+                for (var o = 0; o < 5; o++) {
+                    if (this.getListPionPosition(i, o).length == 0) {
+                        arrayCellEmpty.push("{" + i + ";" + o + "}");
+                        console.log("---{" + i + ";" + o + "}");
+                    }
+                }
+            }
+            return arrayCellEmpty;
+        };
+
+        this.movePions = function (positionStartLine, positionStartColumn, positionStartHeight, positionEndLine, positionEndColumn) {
+            if (this.getListPionPosition(positionEndLine, positionEndColumn).length != 0) {
+                if (this.checkMovePion(positionStartLine, positionStartColumn, positionEndLine, positionEndColumn)) {
+                    var listPionTmp = this.getListPionPosition(positionStartLine, positionStartColumn);
+                    var lstTmp = listPionTmp.slice(positionStartHeight - 1, listPionTmp.length + 1);
+                    for (var i = 0; i < lstTmp.length; i++) {
+                        lstTmp[i].line = positionEndLine;
+                        lstTmp[i].column = positionEndColumn;
+                        lstTmp[i].height = this.getListPionPosition(positionEndLine, positionEndColumn).length;
+                    }
+                    this.checkHeightTower(positionEndLine, positionEndColumn);
+                    if (this.comparaCoup(positionStartLine, positionStartColumn, positionEndLine, positionEndColumn, lstTmp.length)) {
+                        console.log("[ERR] Le coup est identique au précédent !");
+                        return false;
+                    } else {
+                        console.log("Le coup est OK")
+                        this.setLastCoup(positionStartLine, positionStartColumn, positionEndLine, positionEndColumn, lstTmp.length);
+                        return true;
+                    }
+                } else {
+                    console.log("[ERR] Le déplacement est imposible mauvaise distance ou direction !");
+                    return false;
+
+                }
+            }
+            else {
+                console.log("[ERR] Le déplacement est imposible la cellule est VIDE !");
+                return false;
+            }
+        };
+
+        this.changePlayer = function () {
+            if (playerProgress == "W")
+                playerProgress = "B";
+            else
+                playerProgress = "W";
+
+            console.log("C'est au tour du joueur :" + playerProgress);
+        };
+
+        this.getPlayerProgress = function () {
+            return playerProgress;
+        };
+
+        this.checkHeightTower = function (line, column) {
+            var tower = this.getListPionPosition(line, column);
+            var colorLastPion = tower[tower.length - 1].color;
+            if (tower.length >= 5) {
+                if (colorLastPion == colorPlayer)
+                    pointPlayer++;
+                else
+                    pointIA++;
+
+                console.log("Le joueur " + colorLastPion + " a gagner !!");
+                this.deleteTower(line, column);
+            }
+        };
+
+        this.getScorePlayer = function () {
+            return pointPlayer;
+        };
+
+        this.getScoreIA = function () {
+            return pointIA;
+        };
+
+        this.deleteTower = function (line, column) {
+            var tower = this.getListPionPosition(line, column);
+            for (var i = 0; i < listPion.length; i++) {
+                for (var j = 0; j < tower.length; j++) {
+                    if (listPion[i] == tower[j]) {
+                        console.log("SUPPRESSION L:" + listPion[i].line + " C:" + listPion[i].column + " H:" + listPion[i].height + " C:" + listPion[i].color);
+                        listPion.splice(j, 1);
+                    }
                 }
             }
         }
-    }
 
-    this.checkMovePion = function(startLine, startColumn, endLine, endColumn) {
-        console.log("Taille : " + this.getListPionPosition(endLine,endColumn).length)
-        console.log((endColumn-startColumn))
-        if((endColumn-startColumn) <= this.getListPionPosition(endLine,endColumn).length) {
-            console.log("Distance OK !")
-            if(startLine == endLine) {
-                console.log("M�me Ligne");
-                return true;
+        this.checkMovePion = function (startLine, startColumn, endLine, endColumn) {
+            var h = this.getHeightCell(endLine, endColumn);
+            console.log(h)
+            if (Math.abs(startLine - endLine) == Math.abs(startColumn - endColumn == h)
+                || Math.abs(startLine - endLine) == h && startColumn == endColumn || Math.abs(startColumn - endColumn) == h && startLine - endLine) {
+                var deltaX = (startLine == endLine) ? 0 : (endLine > startLine) ? 1 : -1;
+                var deltaY = (startColumn == endColumn) ? 0 : (endColumn > startColumn) ? 1 : -1;
+                for (var x = startLine + deltaX; x !== endLine; x = x + deltaX) {
+                    for (var y = startColumn + deltaY; y !== endColumn; y = y + deltaY) {
+                        console.log("X " + x + " Y " + y);
+                        if (this.getListPionPosition(x, y).length != 0) {
+                            console.log("FALSE");
+                            return false;
+                        } else {
+                            console.log("TRUE");
+                        }
+                    }
+                }
+                if (startLine == endLine) {
+                    return true;
+                }
+                if (startColumn == endColumn) {
+                    return true;
+                }
+                if (Math.abs(startLine - endLine) == Math.abs(startColumn - endColumn)) {
+                    return true;
+                }
+                console.log("False Déplacement")
+                return false //Ne respecte pas les déplacements autorisés
             }
-
-            if(startColumn == endColumn) {
-                console.log("M�me Colonne");
-                return true;
-            }
-
-            if(Math.abs(startLine-endLine) == Math.abs(startColumn-endColumn) ) {
-                console.log("M�me Diagonale");
-                return true;
-            }
-            return false
+            console.log("False Distance")
+            return false; //Mauvaise distance
         }
-        console.log("Aucune correspondance");
-        return false;
+
+        this.getHeightCell = function (line, column) {
+            return this.getListPionPosition(line, column).length;
+        }
+
+        this.setLastCoup = function (positionStartLine, positionStartColumn, positionEndLine, positionEndColumn, nbPions) {
+            lastCoup["startLine"] = positionStartLine;
+            lastCoup["startColumn"] = positionStartColumn;
+            lastCoup["endLine"] = positionEndLine;
+            lastCoup["endColumn"] = positionEndColumn;
+            lastCoup["nbPions"] = nbPions;
+        }
+
+        this.comparaCoup = function (positionStartLine, positionStartColumn, positionEndLine, positionEndColumn, nbPions) {
+            console.log("___Comparaison de coup : ");
+            console.log("___Nouveau coup : Départ : " + positionStartLine + " " + positionStartColumn + " Arrivé : "
+                + positionEndLine + " " + positionEndColumn);
+            console.log("___Ancien coup : Départ : " + lastCoup.startLine + " " + lastCoup.startColumn + " Arrivé : "
+                + lastCoup.endLine + " " + lastCoup.endColumn);
+            if (positionEndLine == lastCoup.startLine && positionEndColumn == lastCoup.startColumn && positionStartLine == lastCoup.endLine
+                && positionStartColumn == lastCoup.endColumn && nbPions == lastCoup.nbPions) {
+                console.log("TRUE")
+            }
+        }
+
     }
-};
+    ;
--- test/test.js
+++ test/test.js
@@ -7,61 +7,14 @@ MixTourTestCase.prototype.testInitPlateau = function() {
     console.log("");
     console.log(" --- INIT PLATEAU ---");
     monPlateau.init();
-
-    monPlateau.setColorPlayer("W");
-
-    console.log("Le joueur en cour est :"+monPlateau.getPlayerProgress());
-
-    console.log("Pion sur le plateau ? : "+monPlateau.pionOnPlateau());
-    /*monPlateau.addPion(0, 0, "G");//1
-    monPlateau.addPion(0, 1, "O");//2
-    monPlateau.addPion(0, 2, "L");//3
-    monPlateau.addPion(0, 3, "F");
-    monPlateau.viewListPionPlateau();
-    */
-
-    //monPlateau.addPion(0, 2, monPlateau.getPlayerProgress());
-    //console.log(monPlateau.checkHeightTower(0,2));
-
-    //monPlateau.addPion(0, 2, monPlateau.getPlayerProgress());
-    //monPlateau.addPion(2, 2, monPlateau.getPlayerProgress());
-    //monPlateau.addPion(3, 2, monPlateau.getPlayerProgress());
-
-    console.log(" TEST DELETE ")
-    monPlateau.viewListPionPosition(0,0);
-    monPlateau.addPion(0,0,monPlateau.getPlayerProgress());//
-    monPlateau.addPion(0,1,monPlateau.getPlayerProgress());
-    monPlateau.addPion(0,2,monPlateau.getPlayerProgress());
-    monPlateau.movePions(0,2,1,0,1);
-    monPlateau.movePions(0,1,1,0,0);
-    monPlateau.viewListPionPosition(0,0);
-    console.log("");
-    console.log("DEUXIEME LIGNE");
-    monPlateau.addPion(1,2,monPlateau.getPlayerProgress());
-    monPlateau.addPion(1,3,monPlateau.getPlayerProgress());
-    monPlateau.addPion(1,4,monPlateau.getPlayerProgress());
-    monPlateau.movePions(1,2,1,1,3);
-    monPlateau.movePions(1,3,1,1,4);
-    monPlateau.viewListPionPosition(1,4);
-    console.log("");
-    console.log("EMPILEMENT");
-    monPlateau.movePions(1,4,1,0,0);
-    console.log(" FIN TEST DELETE ");
-
-    console.log("Score Joueur 1:"+ monPlateau.getScorePlayer());
-    console.log("Score IA:"+ monPlateau.getScoreIA());
-    
     monPlateau.addPion(0, 0, "G");//1
+    monPlateau.addPion(0, 1, "O");//1
 
-    monPlateau.addPion(0, 4, "L");//1
-    monPlateau.addPion(1, 4, "F");//1
-    monPlateau.addPion(2, 4, "V");//1
-    monPlateau.addPion(3, 4, "W");//1
-
-   // monPlateau.movePions(0,0,1,0,4);//Départ en (0;0) on prend les pions à partir de la hauteur 2 et on les déplacenet en (0;1)
-    monPlateau.movePions(3,4,1,0,4)
-    monPlateau.movePions(2,4,1,0,4)
-    monPlateau.movePions(1,4,1,0,4)
-
-    monPlateau.checkMovePion(0,0,0,4);
+    console.log(" --- MOVE ---");
+    monPlateau.movePions(0,0,1,0,1)
+    monPlateau.addPion(0,0,"L")
+    monPlateau.viewListPionPosition(0,1);
+    monPlateau.movePions(0,1,1,0,0)
+    monPlateau.viewListPionPosition(0,0);
+    //console.log(monPlateau.checkMovePion(1,4,0,4));
 };
